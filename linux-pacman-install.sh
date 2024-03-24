ee9249f9ea94230688662a92e4586f639c0af6a8
Dominik <wezik@users.noreply.github.com>
2024-03-24 19:28:33 +0100

    nvm install script

--- linux-pacman-install.sh
+++ linux-pacman-install.sh
@@ -4,10 +4,12 @@ pacman -Sy wezterm zig neovim nvm ripgrep python --noconfirm
 
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
+echo 'source /usr/share/nvm/init-nvm.sh' >> "$USER_HOME/.bashrc"
+
 nvim_repo="https://www.github.com/wezik/nvim-config"
 nvim_branch="refactor/0.2"
 
 git clone -b $nvim_branch $nvim_repo "$USER_HOME/.dotfiles/nvim"
 
-echo "Press Enter to continue..."
+echo "To install NodeJS and NPM restart shell and type in \"nvm install --lts\""
 read -r
