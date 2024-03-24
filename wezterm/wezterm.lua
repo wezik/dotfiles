931df8ef5b400d65799be5fc926f75dfc2010e8c
Dominik <wezik@users.noreply.github.com>
2024-03-24 13:03:00 +0100

    wezterm config update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,9 +4,31 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'Batman'
+config.use_fancy_tab_bar = false
 
 local is_windows = package.config:sub(1,1) == '\\'
 local bg_path
+
+local act = wezterm.action
+
+config.keys = {
+  {
+    key = 'R',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, _, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+  -- other keys
+}
 
 if is_windows then
         local wezterm_home = os.getenv('XDG_CONFIG_HOME')
