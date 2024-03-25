aba42d1bcd46359cec9aa3d413f307edefdf4a62
Dominik <wezik@users.noreply.github.com>
2024-03-25 01:02:32 +0100

    starship added to install

--- linux-pacman-install.sh
+++ linux-pacman-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Sy wezterm zig neovim nvm ripgrep python --noconfirm
+pacman -Sy wezterm zig neovim nvm ripgrep python starship --noconfirm
 
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
@@ -12,4 +12,5 @@ nvim_branch="refactor/0.2"
 git clone -b $nvim_branch $nvim_repo "$USER_HOME/.dotfiles/nvim"
 
 echo "To install NodeJS and NPM restart shell and type in \"nvm install --lts\""
+echo "Press Enter to continue..."
 read -r
--- win-install.bat
+++ win-install.bat
@@ -6,6 +6,7 @@ choco install neovim -y
 choco install nvm -y
 choco install ripgrep -y
 choco install python3 -y
+choco install starship -y
 
 git clone -b "refactor/0.2" "https://github.com/wezik/nvim-config" "%USERPROFLE%\.dotfiles\nvim"
 
