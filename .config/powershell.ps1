5a1f75f10668ce18347716a4da1f98c8d3726bba
Dominik <wezik@users.noreply.github.com>
2024-03-26 23:35:14 +0100

    Powershell update
    okay it was silly..

--- .config/powershell.ps1
+++ .config/powershell.ps1
@@ -1,8 +1,3 @@
-Write-Host "__      _____ _______ _  __"
-Write-Host "\ \    / / __|_  /_ _| |/ /"
-Write-Host " \ \/\/ /| _| / / | || ' < "
-Write-Host "  \_/\_/ |___/___|___|_|\_\"
-Write-Host "=====Powershell session====="
-Write-Host ""
+Write-Host "===New powershell session==="
 # set starship shell
 Invoke-Expression (&starship init powershell)
