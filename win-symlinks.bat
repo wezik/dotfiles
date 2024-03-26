a0cd7f88ae26a0064bd3416295b821801c818a96
Dominik <wezik@users.noreply.github.com>
2024-03-26 21:50:33 +0100

    starship.toml

--- win-symlinks.bat
+++ win-symlinks.bat
@@ -6,6 +6,7 @@ mklink /D "%USERPROFILE%\nvim" "%USERPROFILE%\.dotfiles\nvim"
 setx XDG_CONFIG_HOME "%USERPROFILE%"
 mklink /D "%USERPROFILE%\wezterm" "%USERPROFILE%\.dotfiles\wezterm"
 mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.dotfiles\.gitconfig"
+mklink "%USERPROFILE%\.config\starship.toml" "%USERPROFILE%\.dotfiles\.config\starship.toml"
 
 echo Links created
 pause
