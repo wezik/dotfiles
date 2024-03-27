f9c57e0c5162bc6e72f5b1e4aa95526a47f1c480
Dominik <wezik@users.noreply.github.com>
2024-03-27 22:32:03 +0100

    powershell: added shortcuts

--- .config/powershell.ps1
+++ .config/powershell.ps1
@@ -1,3 +1,11 @@
 Write-Host "===New powershell session==="
 # set starship shell
+
+$q = @{
+        docker = "g:\projects\dockers"
+        db = "g:\projects\dockers\db"
+        pjs = "g:\projects"
+        dotfiles = "$home\.dotfiles"
+}
+
 Invoke-Expression (&starship init powershell)
